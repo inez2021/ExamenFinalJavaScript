9a29ed75508f188736c545ba3fb5d86db1d56441
inez2021 <clara_ines@live.com>
2023-10-01 16:56:08 -0400

    first commit

---
+++ resources/otro_script.js
@@ -0,0 +1,25 @@
+function color(){
+    document.getElementById('p2').style.color="red";
+    document.getElementById('id1').style.backgroundColor="green";
+    document.getElementById('id2').style.backgroundColor="green";
+    document.getElementById('id3').style.backgroundColor="green";
+}
+
+
+function enlace(){
+window.open("https://www.la-razon.com/");  
+}
+
+function consola(){
+    matriz = [['A','B','Color'], ['C','D','Enlace'], ['E','F','Consola']];
+    console. log(matriz[0][0]);
+}
+
+
+
+
+
+
+
+ 
+
